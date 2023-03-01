77f52a8ac55115e28e605606d24e0a9fc6d5a3d6
alexcalister <alister707@gmail.com>
2023-03-01 20:58:40 +0300

    Добавил плагины, breakpoint mb 600px

---
+++ plugins/primevue.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import PrimeVue from 'primevue/config';
+
+// import Button from 'primevue/button';
+// import SelectButton from 'primevue/selectbutton';
+// import InputText from 'primevue/inputtext';
+// import Toast from 'primevue/toast';
+
+// import ToastService from 'primevue/toastservice';
+
+// Vue.component('Button', Button);
+// Vue.component('SelectButton', SelectButton);
+// Vue.component('InputText', InputText);
+// Vue.component('Toast', Toast);
+
+// Vue.use(ToastService);
+Vue.use(PrimeVue, {
+  ripple: true
+});
---
+++ plugins/vee-validate.js
@@ -0,0 +1,43 @@
+import Vue from 'vue';
+import {
+  ValidationObserver,
+  ValidationProvider,
+  extend,
+  setInteractionMode,
+} from 'vee-validate';
+import { required, email, length } from 'vee-validate/dist/rules';
+
+setInteractionMode('eager');
+
+// Register it globally
+Vue.component('ValidationObserver', ValidationObserver);
+Vue.component('ValidationProvider', ValidationProvider);
+
+extend('required', {
+  ...required,
+  message: 'Обязательное поле'
+});
+
+extend('email', {
+  ...email,
+  message: 'Введите корректный email'
+});
+
+extend('length', {
+  ...length,
+  message: 'Заполните поле полностью'
+});
+
+extend('date', {
+  validate: (value) => {
+    return Date.parse(value.split('-').reverse().join('-'));
+  },
+  message: 'Введите корректную дату'
+});
+
+extend('pastdate', {
+  validate: (value) => {
+    return Date.parse(value.split('-').reverse().join('-')) < Date.now();
+  },
+  message: 'Введите корректную дату'
+});
