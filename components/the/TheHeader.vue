8fbbdee91f6de4b3ecf0c8c8dda1b8ebd23eb44e
alexcalister <alister707@gmail.com>
2023-03-01 22:30:32 +0300

    Создал TheHeader

---
+++ components/the/TheHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="header container-wrapper">
+    <div class="header__content container py-20">
+      <AppLogo
+        class="header__logo"
+      />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+  props: {},
+  data: () => ({
+  }),
+  computed: {},
+  methods: {},
+}
+</script>
+
+<style lang="scss">
+.header {
+  /* .header__content */
+  &__content {
+  }
+  /* .header__logo */
+  &__logo {
+  }
+}
+</style>
