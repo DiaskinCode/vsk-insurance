22fa97b812497d8386cf495826cc502783a36abf
alexcalister <alister707@gmail.com>
2023-03-03 17:50:20 +0300

    Добавил Button (prime)

--- plugins/primevue.js
+++ plugins/primevue.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import PrimeVue from 'primevue/config';
 
-// import Button from 'primevue/button';
+import Button from 'primevue/button';
 // import SelectButton from 'primevue/selectbutton';
 // import InputText from 'primevue/inputtext';
 // import Toast from 'primevue/toast';
 
 // import ToastService from 'primevue/toastservice';
 
-// Vue.component('Button', Button);
+Vue.component('Button', Button);
 // Vue.component('SelectButton', SelectButton);
 // Vue.component('InputText', InputText);
 // Vue.component('Toast', Toast);
