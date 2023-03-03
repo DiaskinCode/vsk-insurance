fa9e27cc4cc86dced33b3798655e605f8b557093
alexcalister <alister707@gmail.com>
2023-03-03 17:50:00 +0300

    Добавил AppButton

---
+++ components/app/AppButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <Button
+    class="app-button c-w bg-p py-15 px-30 br-15 fw-7 fs-22 lh-120"
+    v-bind="$attrs"
+    v-on="$listeners"
+  />
+</template>
+
+<script>
+export default {
+  name: 'AppButton',
+  props: {
+  },
+  data: () => ({
+  }),
+  computed: {
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
