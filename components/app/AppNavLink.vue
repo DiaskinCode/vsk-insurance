d4c23be7db922c080b453c635a0cbcad5a9d4ecd
alexcalister <alister707@gmail.com>
2023-03-04 14:16:48 +0300

    Исправил AppNavLink

--- components/app/AppNavLink.vue
+++ components/app/AppNavLink.vue
@@ -1,13 +1,19 @@
 <template>
   <a
-    class="nav-link fs-15 lh-190 us-n"
+    class="nav-link us-n"
     :class="{
       'nav-link--button lh-120 fw-7 c-p py-5 px-15 br-10': button,
     }"
     :href="href"
-    @click="onClick"
   >
-    {{ label }}
+    <AppButton
+      class="nav-link__button"
+      :class="{
+        'nav-link__button--primary': button,
+      }"
+      :label="label"
+      @click="onClick"
+    />
   </a>
 </template>
 
@@ -45,7 +51,23 @@ export default {
   cursor: pointer;
   /* .nav-link--button */
   &--button {
+  }
+  /* .nav-link__button */
+  &__button {
+    background: none;
+    color: $black;
+    line-height: 190%;
+    font-size: 1.5rem;
+    font-weight: 400;
+    padding: 0;
+  }
+  &__button--primary {
     background: #EEF9FF;
+    color: $primary;
+    line-height: 120%;
+    font-weight: 700;
+    padding: 1rem 1.5rem;
+    border-radius: 1rem;
   }
 }
 </style>
