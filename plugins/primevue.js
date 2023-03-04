6ce3c4c06620b273935385d16ada70f7091615b6
alexcalister <alister707@gmail.com>
2023-03-04 17:48:45 +0300

    Добавил Timeline, Toast, ToastService (prime)

--- plugins/primevue.js
+++ plugins/primevue.js
@@ -2,18 +2,17 @@ import Vue from 'vue';
 import PrimeVue from 'primevue/config';
 
 import Button from 'primevue/button';
-// import SelectButton from 'primevue/selectbutton';
-// import InputText from 'primevue/inputtext';
-// import Toast from 'primevue/toast';
+import Timeline from 'primevue/timeline';
 
-// import ToastService from 'primevue/toastservice';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+
 
 Vue.component('Button', Button);
-// Vue.component('SelectButton', SelectButton);
-// Vue.component('InputText', InputText);
-// Vue.component('Toast', Toast);
+Vue.component('Timeline', Timeline);
+Vue.component('Toast', Toast);
 
-// Vue.use(ToastService);
+Vue.use(ToastService);
 Vue.use(PrimeVue, {
   ripple: true
 });
