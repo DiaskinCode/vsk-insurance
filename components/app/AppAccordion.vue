255f8bceed41b03956f2603fdf1e329e909a41d5
alexcalister <alister707@gmail.com>
2023-03-05 11:43:48 +0300

    Добавил AppAccordion

---
+++ components/app/AppAccordion.vue
@@ -0,0 +1,53 @@
+<template>
+  <Accordion
+    class="app-accordion"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <template
+      v-for="(_, slot) of $scopedSlots"
+      v-slot:[slot]="scope"
+    >
+      <slot
+        :name="slot"
+        v-bind="scope"
+      />
+    </template>
+  </Accordion>
+</template>
+
+<script>
+export default {
+  name: 'AppAccordion',
+  props: {},
+  data: () => ({
+  }),
+  computed: {},
+  methods: {},
+}
+</script>
+
+<style lang="scss">
+.p-accordion-header-text {
+  font-weight: 600;
+  font-size: 3.2rem;
+  line-height: 123%;
+  color: $text-15;
+}
+.p-accordion-toggle-icon {
+  display: none;
+}
+.p-accordion-tab {
+  background: $white;
+  box-shadow: 0px .4rem 5rem rgba(0, 0, 0, 0.05);
+  border-radius: 3rem;
+  padding: 5rem;
+  padding-right: 4rem;
+}
+.p-accordion-content {
+  font-size: 2rem;
+  line-height: 140%;
+  color: $text-15;
+  margin-top: 2rem;
+}
+</style>
--- plugins/primevue.js
+++ plugins/primevue.js
@@ -4,6 +4,8 @@ import PrimeVue from 'primevue/config';
 import Button from 'primevue/button';
 import Timeline from 'primevue/timeline';
 import InputText from 'primevue/inputtext';
+import Accordion from 'primevue/accordion';
+import AccordionTab from 'primevue/accordiontab';
 
 import Toast from 'primevue/toast';
 import ToastService from 'primevue/toastservice';
@@ -11,6 +13,8 @@ import ToastService from 'primevue/toastservice';
 Vue.component('Button', Button);
 Vue.component('Timeline', Timeline);
 Vue.component('InputText', InputText);
+Vue.component('Accordion', Accordion);
+Vue.component('AccordionTab', AccordionTab);
 
 Vue.component('Toast', Toast);
 Vue.use(ToastService);
