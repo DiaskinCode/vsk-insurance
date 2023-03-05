e0c3437cfd29f1b7f67fb87281540194c6e0bb2d
alexcalister <alister707@gmail.com>
2023-03-05 10:54:22 +0300

    Добавил AppInput

---
+++ components/app/AppInput.vue
@@ -0,0 +1,49 @@
+<template>
+  <component
+    :is="component"
+    :id="id"
+    class="app-input w-100 bg-w bs-2 br-15 p-20 fs-16 b-1 bc-w"
+    :type="type"
+    :value="value"
+    v-bind="$attrs"
+    v-on="$listeners"
+  />
+</template>
+
+<script>
+export default {
+  name: 'AppInput',
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+    value: {
+      type: String,
+      default: '',
+    },
+    id: {
+      type: String,
+      required: true,
+    },
+    component: {
+      type: String,
+      required: true,
+    },
+  },
+  data: () => ({
+  }),
+  computed: {},
+  methods: {
+  },
+
+}
+</script>
+
+<style lang="scss">
+.app-input {
+  & > * {
+    width: 100%;
+  }
+}
+</style>
--- plugins/primevue.js
+++ plugins/primevue.js
@@ -3,15 +3,18 @@ import PrimeVue from 'primevue/config';
 
 import Button from 'primevue/button';
 import Timeline from 'primevue/timeline';
+import InputText from 'primevue/inputtext';
 
 import Toast from 'primevue/toast';
 import ToastService from 'primevue/toastservice';
 
 Vue.component('Button', Button);
 Vue.component('Timeline', Timeline);
-Vue.component('Toast', Toast);
+Vue.component('InputText', InputText);
 
+Vue.component('Toast', Toast);
 Vue.use(ToastService);
+
 Vue.use(PrimeVue, {
   ripple: true
 });
