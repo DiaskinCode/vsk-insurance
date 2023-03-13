5089816b0836a7029ac8435204f7c66281399bf4
alexcalister <alister707@gmail.com>
2023-03-13 21:22:54 +0300

    Добавил запросы из TheFormCalculate

---
+++ mixins/errors.js
@@ -0,0 +1,35 @@
+export default {
+  data: () => ({
+  }),
+  methods: {
+    showError({
+      detail,
+      summary = 'Ошибка',
+      life = 3000,
+      closable = false,
+      src = '/error.svg',
+    }) {
+      this.$toast.add({
+        src,
+        summary,
+        detail,
+        life,
+        closable,
+        severity: 'error',
+      });
+    },
+    showSuccess({
+      detail,
+      summary = 'Запрос успешно отправлен',
+      life = 3000,
+      closable = false,
+    }) {
+      this.$toast.add({
+        summary,
+        detail,
+        life,
+        severity: 'success',
+      });
+    },
+  },
+};
--- pages/index.vue
+++ pages/index.vue
@@ -4,20 +4,41 @@
     <TheInclude />
     <TheBenefit />
     <TheStep />
-    <TheFormCalculate />
+    <TheFormCalculate
+      @fetch-calculate="fetchCalculate"
+    />
     <TheFormOrder />
     <TheQuestion />
   </div>
 </template>
 
 <script>
+import errorsMixin from '~/mixins/errors';
 export default {
   name: 'PageIndex',
+  mixins: [
+    errorsMixin,
+  ],
   props: {},
   data: () => ({
   }),
   computed: {},
-  methods: {},
+  methods: {
+    async fetchCalculate(data) {
+      const { response, fail } = await this.fetchCalculateAction(data);
+      if (fail) {
+        this.showError({ detail: response.data });
+        return;
+      }
+      this.showSuccess({ detail: response.data });
+    },
+    async fetchCalculateAction(data) {
+      const responseObject = await this.$axios.post('calculator/', data)
+        .then((response) => ({ response, fail: false }))
+        .catch((response) => ({ response, fail: true }));
+      return responseObject;
+    },
+  },
 }
 </script>
 
